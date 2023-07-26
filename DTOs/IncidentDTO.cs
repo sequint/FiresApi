dc68355ecf860983ec1a7b8ef54d665fdabdead0
stevenquintana <steve.e.quint@gmail.com>
2023-07-26 10:34:19 -0700

    Create DTO folder and Indcident DTO

---
+++ DTOs/IncidentDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace FiresApi.Models
+{
+    public class Incident
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public bool Final { get; set; }
+        public string? Updated { get; set; }
+        public string? Started { get; set; }
+        public string? AdminUnit { get; set; }
+        public string? County { get; set; }
+        public string? Location { get; set; }
+        public int AcresBurned { get; set; }
+        public int PercentContained { get; set; }
+        public string? AgencyNames { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string? Type { get; set; }
+        public string? UniqueId { get; set; }
+        public string? Url { get; set; }
+        public string? StartedDateOnly { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
