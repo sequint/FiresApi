4f2c249728c9a28b4ec66dbbf808a8ac992cf219
stevenquintana <steve.e.quint@gmail.com>
2023-07-26 11:12:51 -0700

    Create context db for incidents

---
+++ Context/IncidentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FiresApi.Models;
+
+namespace FiresApi.Context
+{
+	public class IncidentContext : DbContext
+	{
+		public IncidentContext(DbContextOptions<IncidentContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Incident> Incidents { get; set; } = null!;
+	}
+}
