222bfba3a0052084f9cb20794a528e5b5c25b068
stevenquintana <steve.e.quint@gmail.com>
2023-07-26 13:36:32 -0700

    Update id type from string to long

--- Models/Incident.cs
+++ Models/Incident.cs
@@ -2,7 +2,7 @@
 
 public class Incident
 {
-	public string? Id { get; set; }
+	public long Id { get; set; }
 	public string? Name { get; set; }
 	public bool Final { get; set; }
 	public string? Updated { get; set; }
