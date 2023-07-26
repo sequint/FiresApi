0b014fd97946c1e9cfbd4f6aaa458b3b4b62f09d
stevenquintana <steve.e.quint@gmail.com>
2023-07-26 09:42:45 -0700

    Replace route template with route name

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
 namespace FiresApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/TodoItems")]
     [ApiController]
     public class TodoItemsController : ControllerBase
     {
